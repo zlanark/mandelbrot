4265370a5bdd279c5930abb01a0c8b88ae26b6cf
zlanark <zlanark@proton.me>
2024-02-29 22:51:43 +1000

    partway through refactoring render system

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <thread>
 #include <future>
+#include <bits/stdc++.h>
 
 #include "matrix.h"
 #include "imgWriter.h"
@@ -49,27 +50,23 @@ double getNormalisedIterations(const u_int & max_depth, const double& escape_rad
 
 class view {
     public:
-        view(int resX, int resY, double rotation, complex<double> topLeft, complex<double> bottomRight, int maxIterations, double escapeRadius) 
-            : resX(resX), resY(resY), rotation(std::fmod(rotation, 360)), BR(bottomRight), TL(topLeft), maxIterations(maxIterations), escapeRadius(escapeRadius), \
+        view(int resX, int resY, complex<double> topLeft, complex<double> bottomRight, int maxIterations, double escapeRadius) 
+            : resX(resX), resY(resY), BR(bottomRight), TL(topLeft), maxIterations(maxIterations), escapeRadius(escapeRadius), \
             xDiv(double(std::real(bottomRight) - std::real(topLeft)) / double(resX)), \
-            yDiv(double(std::imag(topLeft) - std::imag(bottomRight)) / double(resY)),
+            yDiv(double(std::imag(topLeft) - std::imag(bottomRight)) / double(resY)), \
             totalPixels(resX*resY)
-        {
-            // how far to the right (real) and down (imag) you need to go to get from TL to BR
-            complex<double> H = bottomRight - topLeft;
-            double tau = 90 - rotation;
-            
-        }
+        {}
 
         matrix<double> capture();
+
         matrix<double> parallel_capture();
+        matrix<double> parallel_capture(const int batchSize);
 
     private:
-        void pixel_worker(std::atomic<int>& pixel_counter, std::mutex& pixel_counter_mutex, matrix<double>& img);
+        void pixel_worker(std::atomic<int>& batchCounter, std::mutex& batchCounterMutex, const int batchSize, matrix<double>& img);
 
-        complex<double> xDiv;
-        complex<double> yDiv;
-        const double rotation;
+        const double xDiv;
+        const double yDiv;
         const int resX;
         const int resY;
         const complex<double> TL;
@@ -96,46 +93,68 @@ matrix<double> view::capture() {
 }
 
 matrix<double> view::parallel_capture() {
+    unsigned int numThreads = std::thread::hardware_concurrency();
+    numThreads = (numThreads == 0 ? 1 : numThreads);
+    const int batchSize = (numThreads > 1 ? this->totalPixels / (4*numThreads) : totalPixels);
+
+    return view::parallel_capture(batchSize);
+}
+
+matrix<double> view::parallel_capture(const int batchSize) {
     matrix<double> img (resX, resY, 0);
     if(real(TL) >= real(BR) || imag(TL) <= imag(BR)) {
         // image has no area (negative dimensions not allowed). Return empty image.
         return img;
     }
 
-    unsigned int num_threads = std::thread::hardware_concurrency();
-    num_threads = (num_threads == 0 ? 1 : num_threads);
-
+    unsigned int numThreads = std::thread::hardware_concurrency();
+    numThreads = (numThreads == 0 ? 1 : numThreads);
+    
     std::vector<std::thread> threads;
-    std::mutex pixel_counter_mutex;
-    std::atomic<int> pixel_counter = 0;
+    std::mutex batchCounterMutex;
+    std::atomic<int> batchCounter = 0;
 
-    for(int x=0; x < num_threads; x++){
-        threads.push_back(std::thread(&view::pixel_worker, this, std::ref(pixel_counter), std::ref(pixel_counter_mutex), std::ref(img)));
+    for(int x=0; x < numThreads; x++){
+        threads.push_back(std::thread(&view::pixel_worker, this, std::ref(batchCounter), std::ref(batchCounterMutex), batchSize, std::ref(img)));
     }
     for(std::thread& t : threads) t.join();
     return img;
 }
 
-void view::pixel_worker(std::atomic<int>& pixel_counter, std::mutex& pixel_counter_mutex, matrix<double>& img) {
+void view::pixel_worker(std::atomic<int>& batchCounter, std::mutex& batchCounterMutex, const int batchSize, matrix<double>& img) {
     while(true) {
-        std::unique_lock lock(pixel_counter_mutex);
-        if(pixel_counter >= this->totalPixels) break;
-        int pixel_num = pixel_counter;
-        pixel_counter++;
+        // Get the next available batch number and increment
+        std::unique_lock lock(batchCounterMutex);
+        int batch = batchCounter;
+        batchCounter++;
         lock.unlock();
 
-        int x = pixel_num % this->resX;
-        int y = pixel_num / this->resX;
-        complex<double> c {(real(this->TL) + x*this->xDiv), (imag(this->TL) - y*this->yDiv)};
-
-        img[y][x] = getNormalisedIterations(this->maxIterations, this->escapeRadius, c);
+        unsigned int numPixels = batchSize;
+        // Would the last pixel in this batch be outside image bounds?
+        if((batchCounter+1)*batchSize > this->totalPixels) {
+            // By how much?
+            unsigned int overshoot = (batchCounter+1)*batchSize - totalPixels;
+            // If we haven't overshot by at least 1 batchSize, then there are still `batchSize - overshoot` pixels unrendered
+            if(overshoot < batchSize) numPixels = batchSize - overshoot;
+            else break;
+        };
+        
+        unsigned int lowerBound = batch * batchSize;
+        unsigned int upperBound = lowerBound + numPixels;
+        // Render each pixel in batch
+        for(unsigned int pixelCounter=lowerBound; pixelCounter < upperBound; pixelCounter++) {
+            int x = pixelCounter % this->resX;
+            int y = pixelCounter / this->resX;
+            complex<double> c {(real(this->TL) + x*this->xDiv), (imag(this->TL) - y*this->yDiv)};
+            img[y][x] = getNormalisedIterations(this->maxIterations, this->escapeRadius, c);
+        }
     }
     std::this_thread::yield();
 }
 
 int main(int argc, char *argv[]) {
-    int resX = 2048;
-    int resY = 2048;
+    int resX = 4096;
+    int resY = 4096;
     complex<double> topLeft{-1.3,0.34};
     complex<double> bottomRight{-1.1,0.14};
     // complex<double> topLeft {-2, ((double) 4/dimX)*(dimY/2)};
@@ -144,7 +163,7 @@ int main(int argc, char *argv[]) {
     double escapeRadius = 3.1;
 
     // matrix<double> mandelbrot_result = mandelbrot(dimX, dimY, topLeft, bottomRight, maxIterations, escapeRadius);
-    matrix<double> mandelbrot_result = view(resX, resY, 0, topLeft, bottomRight, maxIterations, escapeRadius).parallel_capture();
+    matrix<double> mandelbrot_result = view(resX, resY, topLeft, bottomRight, maxIterations, escapeRadius).parallel_capture(4096);
     matrix<RGBpixel> image{resX, resY, RGBpixel{0,0,0}};
 
     for(int y=0; y<resY; y++){
@@ -153,7 +172,7 @@ int main(int argc, char *argv[]) {
             double factor = iterations/maxIterations; //(double)2/3
             // double blue = (double) 1 / double(1 + pow(( factor / double(1-factor)), -1) );
             double blue = sqrt(factor);
-            image[y][x] = (iterations < 0 ? RGBpixel{255,255,255} : RGBpixel{int(255*sqrt(pow(factor,1.5))),int(255*factor),int(255*blue)});
+            image[y][x] = (iterations < 0 ? RGBpixel{255,255,255} : RGBpixel{u_int8_t(255*sqrt(pow(factor,1.5))),u_int8_t(255*factor),u_int8_t(255*blue)});
         }
     }
 
--- mandelbrot.h
+++ mandelbrot.h
@@ -23,67 +23,127 @@ class mandelbrot {
 
         matrix<double> capture();
         matrix<double> parallel_capture();
+        matrix<double> parallel_capture(const int batchSize);
 
     private:
-        void pixel_worker(std::atomic<int>& pixel_counter, std::mutex& pixel_counter_mutex, matrix<double>& img);
+        void pixel_worker(std::atomic<int>& batchCounter, std::mutex& batchCounterMutex, const int batchSize, matrix<double>& img);
+
+        double getNormalisedIterations(const unsigned int& max_depth, const double& escape_radius, const std::complex<double>& c);
+        unsigned int getIterations(const unsigned int & max_depth, const double& escape_radius, const std::complex<double>& c);
+        unsigned int getIterationsRecursive(unsigned int depth, const double& escape_radius, std::complex<double>& z, const std::complex<double>& c);
 
         view camera;
         const int maxIterations;
         const double escapeRadius;
 };
 
-matrix<double> view::capture() {
-    matrix<double> img (resX, resY, 0);
-    if(real(TL) >= real(BR) || imag(TL) <= imag(BR)) {
-        // image has no area (negative dimensions not allowed). Return empty image.
-        return img;
+/* Recursive version of getIterations() */
+unsigned int mandelbrot::getIterationsRecursive(unsigned int depth, const double& escape_radius, std::complex<double>& z, const std::complex<double>& c) {
+    if (std::abs(z) > escape_radius){
+        // Not in set
+        return depth;
     }
+    if(depth <= 0){
+        // Might be in set
+        return -1;
+    }
+    z = z*z + c;
+    return getIterationsRecursive(depth-1, escape_radius, z, c);
+}
 
-    for(int y=0; y<resY; y++) {
-        for(int x=0; x<resX; x++) {
-            complex<double> c {(real(TL) + x*xDiv), (imag(TL) - y*yDiv)};
-            img[y][x] = getNormalisedIterations(maxIterations, escapeRadius, c);
+/* Will return -1 if the pixel diverges, otherwise # iterations */
+unsigned int mandelbrot::getIterations(const unsigned int & max_depth, const double& escape_radius, const std::complex<double>& c){
+    std::complex<double> z = 0;
+    int iters_left = getIterationsRecursive(max_depth, escape_radius, z, c);
+    return (iters_left < 0 ? -1 : max_depth - iters_left);
+}
+
+double mandelbrot::getNormalisedIterations(const unsigned int& max_depth, const double& escape_radius, const std::complex<double>& c) {
+    std::complex<double> z = 0;
+    u_int iterations = 0;
+    while(abs(z) <= escape_radius && iterations < max_depth) {
+        z = z*z + c;
+        iterations += 1;
+    }
+    if(iterations == max_depth){
+        return -1;
+    }
+    return iterations - log(log2(abs(z)));
+}
+
+matrix<double> mandelbrot::capture() {
+    matrix<double> img (camera.resX, camera.resY, 0);
+    // if(real(TL) >= real(BR) || imag(TL) <= imag(BR)) {
+    //     // image has no area (negative dimensions not allowed). Return empty image.
+    //     return img;
+    // }
+
+    for(int y=0; y<camera.resY; y++) {
+        for(int x=0; x<camera.resX; x++) {
+            std::array<double, 2> c = camera.pixel2world(x,y);
+            img[y][x] = getNormalisedIterations(maxIterations, escapeRadius, complex<double>{c[0],c[1]});
         }
     }
     return img;
 }
 
-matrix<double> view::parallel_capture() {
-    matrix<double> img (resX, resY, 0);
-    if(real(TL) >= real(BR) || imag(TL) <= imag(BR)) {
-        // image has no area (negative dimensions not allowed). Return empty image.
-        return img;
-    }
+matrix<double> mandelbrot::parallel_capture() {
+    unsigned int numThreads = std::thread::hardware_concurrency();
+    numThreads = (numThreads == 0 ? 1 : numThreads);
+    const int batchSize = (numThreads > 1 ? camera.totalPixels / (4*numThreads) : camera.totalPixels);
 
-    unsigned int num_threads = std::thread::hardware_concurrency();
-    num_threads = (num_threads == 0 ? 1 : num_threads);
+    return mandelbrot::parallel_capture(batchSize);
+}
 
+matrix<double> mandelbrot::parallel_capture(const int batchSize) {
+    matrix<double> img (camera.resX, camera.resY, 0);
+    // if(real(TL) >= real(BR) || imag(TL) <= imag(BR)) {
+    //     // image has no area (negative dimensions not allowed). Return empty image.
+    //     return img;
+    // }
+
+    unsigned int numThreads = std::thread::hardware_concurrency();
+    numThreads = (numThreads == 0 ? 1 : numThreads);
+    
     std::vector<std::thread> threads;
-    std::mutex pixel_counter_mutex;
-    std::atomic<int> pixel_counter = 0;
+    std::mutex batchCounterMutex;
+    std::atomic<int> batchCounter = 0;
 
-    for(int x=0; x < num_threads; x++){
-        threads.push_back(std::thread(&view::pixel_worker, this, std::ref(pixel_counter), std::ref(pixel_counter_mutex), std::ref(img)));
+    for(int x=0; x < numThreads; x++){
+        threads.push_back(std::thread(&mandelbrot::pixel_worker, this, std::ref(batchCounter), std::ref(batchCounterMutex), batchSize, std::ref(img)));
     }
     for(std::thread& t : threads) t.join();
     return img;
 }
 
-void view::pixel_worker(std::atomic<int>& pixel_counter, std::mutex& pixel_counter_mutex, matrix<double>& img) {
+void mandelbrot::pixel_worker(std::atomic<int>& batchCounter, std::mutex& batchCounterMutex, const int batchSize, matrix<double>& img) {
     while(true) {
-        std::unique_lock lock(pixel_counter_mutex);
-        if(pixel_counter >= this->totalPixels) break;
-        int pixel_num = pixel_counter;
-        pixel_counter++;
+        // Get the next available batch number and increment
+        std::unique_lock lock(batchCounterMutex);
+        int batch = batchCounter;
+        batchCounter++;
         lock.unlock();
 
-        int x = pixel_num % this->resX;
-        int y = pixel_num / this->resX;
-        complex<double> c {(real(this->TL) + x*this->xDiv), (imag(this->TL) - y*this->yDiv)};
-
-        img[y][x] = getNormalisedIterations(this->maxIterations, this->escapeRadius, c);
+        unsigned int numPixels = batchSize;
+        // Would the last pixel in this batch be outside image bounds?
+        if((batchCounter+1)*batchSize > camera.totalPixels) {
+            // By how much?
+            unsigned int overshoot = (batchCounter+1)*batchSize - camera.totalPixels;
+            // If we haven't overshot by at least 1 batchSize, then there are still `batchSize - overshoot` pixels unrendered
+            if(overshoot < batchSize) numPixels = batchSize - overshoot;
+            else break;
+        };
+        
+        unsigned int lowerBound = batch * batchSize;
+        unsigned int upperBound = lowerBound + numPixels;
+        // Render each pixel in batch
+        for(unsigned int pixelCounter=lowerBound; pixelCounter < upperBound; pixelCounter++) {
+            int x = pixelCounter % camera.resX;
+            int y = pixelCounter / camera.resX;
+            std::array<double, 2> c = camera.pixel2world(x,y);
+            img[y][x] = getNormalisedIterations(this->maxIterations, this->escapeRadius, complex<double>{c[0],c[1]});
+        }
     }
     std::this_thread::yield();
 }
-
 #endif
